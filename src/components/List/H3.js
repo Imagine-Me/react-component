0b02fcf52d2960ee4c580f6b018bdaeefb3dec83
Prince Thomas <prince.thomas@keleno.com>
2020-02-05 16:57:33 +0530

    Completed adding the data for the list

--- src/components/List/H3.js
+++ src/components/List/H3.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const h3 = (props) => {
+    console.log(props.key)
     return <h3 style={{ color: props.color}}>{props.children}</h3>
 }
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,10 +2,11 @@ import React from 'react';
 import H3 from './H3';
 
 const list = (props) => {
-    return <div>
-        <H3 color="red" >Hello</H3>
-        <H3 color="green">There</H3>
-    </div>
+    const array_list =[]
+    array_list.push(<H3 key={1} color="red" >Hello</H3>)
+    array_list.push(<H3 key={2} color="green">There</H3>)
+
+    return array_list
 }
 
 export default list;
