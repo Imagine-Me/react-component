aa60161552dd6b664c421fa08323b3397216f8a1
Prince <prince100thomas@gmail.com>
2020-02-08 11:57:44 +0530

    Completed events in class component

--- src/components/List/H3.js
+++ src/components/List/H3.js
@@ -1,8 +1,38 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
-class H3 extends Component{
-    render(){
-        return <h3 style={{ color: this.props.color}}>
+class H3 extends Component {
+
+    // value = "H3 clicked"
+
+    //Initial
+    // clickHandler = () => {
+    //     console.log("H3 is clicked")
+    // }
+
+    // accessing event
+    // clickHandler = (event) => {
+    //     console.log(event)
+    // }
+
+    // Passing values
+    clickHandler = (color) => {
+        console.log(color)
+    }
+
+
+    // Normal method
+    // clickHandler() {
+    //     console.log(this.value)
+    // }
+
+    render() {
+        // return <h3 onClick={this.clickHandler} style={{ color: this.props.color}}>
+        //     {this.props.children}
+        // </h3>
+        // return <h3 onClick={this.clickHandler.bind(this,this.props.color)} style={{ color: this.props.color}}>
+        //     {this.props.children}
+        // </h3>
+        return <h3 onClick={() => this.clickHandler(this.props.color)} style={{ color: this.props.color }}>
             {this.props.children}
         </h3>
     }
