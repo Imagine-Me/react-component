6db6ba6aeaddb3bdad533857a4fb0ea9c3e2db0d
Prince <prince100thomas@gmail.com>
2020-02-09 18:18:31 +0530

    event handling completed

--- src/components/List/H3.js
+++ src/components/List/H3.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const clickHandler = () => {
-    console.log("h3 clicked")
-}
 
 const h3 = (props) => {
     // const clickHandler = () => {
     //     console.log("h3 clicked")
     // }
 
-    return <h3 onClick={clickHandler} style={{ color: props.color}}>{props.children}</h3>
+    // const clickHandler = (event) => {
+    //     console.log(event)
+    // }
+    const clickHandler = (color) => {
+        console.log(color)
+    }
+
+
+    // return <h3 onClick={clickHandler} style={{ color: props.color}}>{props.children}</h3>
+    // return <h3 onClick={clickHandler.bind(this,props.color)} style={{ color: props.color}}>{props.children}</h3>
+    return <h3 onClick={() => clickHandler(props.color)} style={{ color: props.color}}>{props.children}</h3>
 }
 
 export default h3;
